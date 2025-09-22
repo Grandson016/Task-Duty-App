31f06c4179f2942551b7d913825a554f35e1b160
Grandson016 <nifextohbad@gmail.com>
2025-09-22 19:28:31 +0100

    latest commit

--- src/pages/AllTasks.jsx
+++ src/pages/AllTasks.jsx
@@ -1,5 +1,5 @@
 import React from 'react' ;
-import addSymbol from "../assets/addsymbol.svg" ;
+import addSymbol from "../assets/addSymbol.svg" ;
 import { Link } from 'react-router-dom' ;
 import { taskData } from '../db' ;
 import MappedTasks from '../components/MappedTasks' ;
